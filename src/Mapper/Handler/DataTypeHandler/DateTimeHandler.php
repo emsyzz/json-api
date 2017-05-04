0847ffcf6026051d1cbd71ff0ef5c0f77a74b846
Michael Mirtenbaum <michael@smithers.corp.local>
2017-05-04 16:48:58 -0400

    DateTimeHandler added to mapper

---
+++ src/Mapper/Handler/DataTypeHandler/DateTimeHandler.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types = 1);
+
+namespace Mikemirten\Component\JsonApi\Mapper\Handler\DataTypeHandler;
+
+/**
+ * DateTime handler
+ *
+ * @package Mikemirten\Component\JsonApi\Mapper\Handler\DataTypeHandler
+ */
+class DateTimeHandler implements DataTypeHandlerInterface
+{
+    const PARAMETER_FORMAT = 0;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toResource($value, array $parameters)
+    {
+        if (! $value instanceof \DateTimeInterface) {
+            $value = new \DateTimeImmutable((string) $value);
+        }
+
+        if (isset($parameters[self::PARAMETER_FORMAT])) {
+            return $value->format($parameters[self::PARAMETER_FORMAT]);
+        }
+
+        return $value->format(DATE_RFC3339);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fromResource($value)
+    {
+        if ($value instanceof \DateTimeInterface) {
+            return $value;
+        }
+
+        return new \DateTimeImmutable((string) $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(): array
+    {
+        return ['datetime'];
+    }
+}
---
+++ tests/Mapper/Handler/DataTypeHandler/DateTimeHandlerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Mikemirten\Component\JsonApi\Mapper\Handler;
+
+use Mikemirten\Component\JsonApi\Mapper\Handler\DataTypeHandler\DateTimeHandler;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group   mapper
+ * @package Mikemirten\Component\JsonApi\Mapper\Handler
+ */
+class DateTimeHandlerTest extends TestCase
+{
+    public function testToResourceObject()
+    {
+        $handler  = new DateTimeHandler();
+        $datetime = new \DateTimeImmutable('2010-01-02');
+
+        $result = $handler->toResource($datetime, ['Y-m-d']);
+
+        $this->assertSame('2010-01-02', $result);
+    }
+
+    public function testToResourceString()
+    {
+        $handler = new DateTimeHandler();
+        $result  = $handler->toResource('2010-02-01', ['Y-d-m']);
+
+        $this->assertSame('2010-01-02', $result);
+    }
+
+    public function testFromResourceObject()
+    {
+        $object = new \DateTimeImmutable();
+
+        $handler = new DateTimeHandler();
+        $result  = $handler->fromResource($object);
+
+        $this->assertInstanceOf('DateTimeInterface', $result);
+        $this->assertSame($object, $result);
+    }
+
+    public function testFromResourceString()
+    {
+        $handler = new DateTimeHandler();
+        $result  = $handler->fromResource('2010-01-02');
+
+        $this->assertInstanceOf('DateTimeInterface', $result);
+        $this->assertSame('2010-01-02', $result->format('Y-m-d'));
+    }
+}
